95bb810e07ddfdd71795091b43d70d22a5041842
Christopher Buhendwa <christopherbuhendwa@Christophers-MacBook-Pro.local>
2024-01-17 18:00:07 +0900

    add project to github

---
+++ src/basics_all/arrayss.java
@@ -0,0 +1,35 @@
+package basics_all;
+
+import java.util.Arrays;
+
+public class arrayss {
+	public static void main(String[] args) {
+		int[] arr;
+		int arr2[];
+			
+		arr = new int[10];
+		System.out.println(arr[0]);
+		
+		double[] arr3 = new double[10];
+		for(int i = 0; i < 10; i++) {
+			arr3[i] = (double)i;
+//			System.out.println(arr3[i]);
+		}
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(arr3[i]);
+		}
+		
+		System.out.println(Arrays.toString(arr3));
+		
+		
+		int[] arr5 = {3,2,1};
+		
+		int[][] matrix = {
+				{1,2,3},
+				{3,4,5}
+		};
+		
+	}
+
+}
