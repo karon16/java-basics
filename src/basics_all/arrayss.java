a748e21f64a62666644e2b37a693f016d26ee86a
Christopher Buhendwa <christopherbuhendwa@Christophers-MacBook-Pro.local>
2024-01-18 15:55:53 +0900

    working on arrays

--- src/basics_all/arrayss.java
+++ src/basics_all/arrayss.java
@@ -26,9 +26,16 @@ public class arrayss {
 		int[] arr5 = {3,2,1};
 		
 		int[][] matrix = {
-				{1,2,3},
-				{3,4,5}
+				{1,2,3,34,11,12},
+				{3,4,5,4,0,42,34,12}
 		};
+		for(int i = 0; i < 2; i++) {
+			Arrays.sort(matrix[i]);
+			System.out.println(Arrays.toString(matrix[i]));
+		}
+		
+		
+		
 		
 	}
 
