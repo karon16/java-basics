8839932adcb229e1d64e40c28ebc93952ce0557a
Christopher Buhendwa <christopherbuhendwa@Christophers-MacBook-Pro.local>
2024-01-19 12:29:51 +0900

    feature : work on strings

---
+++ src/basics_all/strings.java
@@ -0,0 +1,9 @@
+package basics_all;
+
+public class strings {
+    public static void main(String[] args){
+        String s = " hello ";
+        System.out.println(s);
+        System.out.println(s.toUpperCase());
+    }
+}
